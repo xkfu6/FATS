ba5ddf4130762a645843b89470e59fc3d4d0c94d
xz <12597103+xzfu@user.noreply.gitee.com>
2024-09-26 09:26:46 +0800

    Revert "FAST"
    This reverts commit 19a042450f9aa22bfc7a420adfa280f57d76f0cb.

--- fast/application/home/controller/Business.php
+++ fast/application/home/controller/Business.php
@@ -2,33 +2,16 @@
 
 namespace app\home\controller;
 
-// 前台公共控制器
-use app\common\controller\Home;
+use think\Controller;
+use think\Request;
 
-class Business extends Home
+class Business extends Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->BusinessModel = model('common/Business/Business');
     }
-    // 我的
     public function index()
-    {
-        return $this->view->fetch();
-    }
-    // 联系我们
-    public function contact()
-    {
-        //模板渲染显示
-        return $this->view->fetch();
-    }
-    public function collection()
-    {
-        return $this->view->fetch();
-    }
-    // 基本资料
-    public function profile()
     {
         return $this->view->fetch();
     }
--- fast/application/home/controller/Index.php
+++ fast/application/home/controller/Index.php
@@ -2,92 +2,20 @@
 
 namespace app\home\controller;
 
-use app\common\controller\Home;
+//让我们自己的控制器去继承Thinkphp的控制器'
+//面向对象引入
+use \think\Controller; //把TP的底层控制器引入进来
 
-class Index extends Home
+class Index extends Controller
 {
-    public $NoLogin = ['*'];
-
     public function __construct()
     {
-
         // 调用父类的构造
         parent::__construct();
-
-        $this->BussinessModel = model('common/Business/Business');
-        $this->SourceModel = model('common/Business/Source');
     }
-
-    // 主页
     public function index()
     {
         //模板渲染显示
         return $this->view->fetch();
-    }
-
-    // 登录
-    public function login()
-    {
-        if ($this->request->isPost()) {
-            $mobile = $this->request->param('mobile', '', 'trim');
-            $password = $this->request->param('password', '', 'trim');
-            if (empty($mobile)) {
-                $this->error('请填写手机号');
-                exit;
-            }
-            if (empty($password)) {
-                $this->error('请填写密码');
-                exit;
-            }
-            $resl = $this->BussinessModel->where(['mobile' => $mobile])->find();
-
-            if ($resl) {
-                $salt = $resl['salt'];
-                $password = md5($password . $salt);
-                if ($resl['password'] != $password) {
-                    $this->error('密码不正确');
-                    exit;
-                }
-                cookie('busid', $resl['id']);
-                cookie('mobile', $resl['mobile']);
-                $this->success('登录成功', url('home/business/index'));
-                exit;
-            } else {
-                $salt = build_randstr();
-                $password = md5($password . $salt);
-                $sourceid = $this->SourceModel->where(['name' => ['LIKE', "%云课堂%"]])->value('id');
-                $pata = [
-                    'mobile' => $mobile,
-                    'nickname' => build_encrypt($mobile),
-                    'password' => $password,
-                    'salt' => $salt,
-                    'gender' => 0,
-                    'sourceid' => $sourceid,
-                    'deal' => '0',
-                    'money' => '0',
-                    'auth' => '0',
-                ];
-                // $result = $User->validate('Member')->save($data);    
-                // 调用验证器 
-                $result = $this->BussinessModel->validate('common/Business/Business')->save($pata);
-                if (false === $result) {
-                    // 验证失败 输出错误信息
-                    $this->error($this->BussinessModel->getError());
-                    exit;
-                }
-                cookie('busid', $resl['id']);
-                cookie('mobile', $resl['mobile']);
-                $this->success('注册成功', url('home/business/index'));
-                exit;
-            }
-        }
-
-        return $this->view->fetch();
-    }
-    // 退出
-    public function outlogin()
-    {
-        cookie(null, 'fa_');
-        $this->redirect(url('home/index/login'));
     }
 }
